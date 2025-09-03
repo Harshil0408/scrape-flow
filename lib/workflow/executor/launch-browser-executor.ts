78cc1f342a6bc7196a8634091afa13e440577936
Harshil0408 <kanjariyaharshit@gmail.com>
2025-09-04 00:01:56 +0530

    feat: add Puppeteer integration and update Workflow schema
    - Added Puppeteer as a dependency for browser automation.
    - Implemented LaunchBrowserExecutor to launch and close a browser instance.
    - Registered LaunchBrowserExecutor in the executor registry.
    - Updated Workflow model in Prisma schema to include lastRunAt, lastRunId, and lastRunStatus fields.
    - Added creditsConsumed field to WorkflowExecution model.
    - Created migration scripts to alter existing tables and redefine WorkflowExecution schema.
    - Defined Environment type to include browser and phases for executor functions.

---
+++ lib/workflow/executor/launch-browser-executor.ts
@@ -0,0 +1,20 @@
+import { waitFor } from "@/lib/helper/waitfor";
+import { Environment } from "@/types/executor";
+import puppeteer from "puppeteer";
+
+export async function LaunchBrowserExecutor(environment: Environment): Promise<boolean> {
+    try {
+        console.log("@ENV", environment)
+        const browser = await puppeteer.launch({
+            headless: false,
+        });
+
+        await waitFor(3000);
+        await browser.close();
+
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
